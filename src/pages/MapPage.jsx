2c4dc5037de444081de60eba66a7afed207b6a5e
Yashovardhan A <yashrailfansbc@gmail.com>
2026-06-23 17:26:40 +0530

    add

--- src/pages/MapPage.jsx
+++ src/pages/MapPage.jsx
@@ -1,3 +1,17 @@
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: markerIcon2x,
+    iconUrl: markerIcon,
+    shadowUrl: markerShadow,
+});
 import { useEffect, useState } from "react";
 import api from "../services/api";
 import {
