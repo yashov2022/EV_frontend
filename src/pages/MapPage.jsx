254d6563997d5bc6fd3eb97b1741c55887b7ea64
Yashovardhan A <yashrailfansbc@gmail.com>
2026-06-23 19:23:05 +0530

    add

--- src/pages/MapPage.jsx
+++ src/pages/MapPage.jsx
@@ -1,10 +1,20 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import api from "../services/api";
+
+import {
+    MapContainer,
+    TileLayer,
+    Marker,
+    Popup
+} from "react-leaflet";
+
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 
 import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
 import markerIcon from "leaflet/dist/images/marker-icon.png";
 import markerShadow from "leaflet/dist/images/marker-shadow.png";
-import { useNavigate } from "react-router-dom";
 
 delete L.Icon.Default.prototype._getIconUrl;
 
@@ -13,85 +23,47 @@ L.Icon.Default.mergeOptions({
     iconUrl: markerIcon,
     shadowUrl: markerShadow,
 });
-import { useEffect, useState } from "react";
-import api from "../services/api";
-import {
-    MapContainer,
-    TileLayer,
-    Marker,
-    Popup
-} from "react-leaflet";
-import { useNavigate } from "react-router-dom";
-import slots from "./Slots";
 
 function MapPage() {
 
     const [stations, setStations] = useState([]);
     const navigate = useNavigate();
 
-   useEffect(() => {
-
-    api
-        .get("/stations")
-        .then((response) => {
-
-            setStations(response.data);
-
-        });
-
-}, []);
+    useEffect(() => {
+        api.get("/stations")
+            .then((response) => {
+                setStations(response.data);
+            });
+    }, []);
 
     return (
-
         <MapContainer
             center={[12.9716, 77.5946]}
             zoom={12}
-            style={{
-                height: "600px",
-                width: "100%"
-            }}
+            style={{ height: "600px", width: "100%" }}
         >
-
             <TileLayer
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
 
-           
+            {stations.map((station) => (
+                <Marker
+                    key={station.id}
+                    position={[station.latitude, station.longitude]}
+                >
+                    <Popup>
+                        <h3>{station.name}</h3>
+                        <p>{station.address}</p>
 
-            {
-                stations.map((station) => (
-
-                    <Marker
-                        key={station.id}
-                        position={[
-                            station.latitude,
-                            station.longitude
-                        ]}
-                    >
-
-                        <Popup>
-
-               <h3>{station.name}</h3>
-
-              <p>{station.address}</p>
-
-             <button
-                 onClick={() =>
-            navigate(`/slots/${station.id} `)
-                  }
-              >
-        View Slots
-    </button>
-
-</Popup>
-
-                    </Marker>
-
-                ))
-            }
-
+                        <button
+                            onClick={() => navigate(`/slots/${station.id}`)}
+                        >
+                            View Slots
+                        </button>
+                    </Popup>
+                </Marker>
+            ))}
         </MapContainer>
-
     );
 }
 
